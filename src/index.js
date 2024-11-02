e8a35a138556bf518dd14de8336ea2da852659bb
Abhinav Singh <adikari.abhinav@gmail.com>
2024-11-02 18:04:47 +0530

    fixed register controller bugs

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./.env",
+});
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000, ()=>{
-        console.log(` Server is running on port: ${process.env.PORT}`)
-    })
-})
-.catch((err)=>{
-    console.log("Mongo DB Connection failed !!", err)
-})
-
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(` Server is running on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo DB Connection failed !!", err);
+  });
 
 /* Approach to connect DB
 import express from "express"
